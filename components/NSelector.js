5ef27f758cd879cc788ede3090ad0607db8a84b4
Aryan Chavan <aryan@aryansdomain.com>
2021-07-04 13:02:41 +0530

    add numeric input to NSelector

--- components/NSelector.js
+++ components/NSelector.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { Text, View, TextInput } from "react-native";
+import NumericInput from "react-native-numeric-input";
 
 function NSelector({ nVariables, setNVariables }) {
 	function updateNVariables(newN) {
 		if (!isNaN(newN)) {
-			if (newN <= 16 && newN > 0) setNVariables(newN);
+			if (newN <= 25 && newN > 0) setNVariables(newN);
+			else newN = 3;
 		}
 	}
 
 	return (
 		<View>
 			<Text>enter number of variables</Text>
+			{/* the old textinput is there for browser debugging */}
 			<TextInput
 				style={{ borderWidth: 1, borderRadius: 100, paddingLeft: 12 }}
 				defaultValue={3}
 				onChangeText={updateNVariables}
+			/>
+			<NumericInput
+				value={nVariables}
+				minValue={1}
+				maxValue={25}
+				step={1}
+				onChange={updateNVariables}
 			/>
 		</View>
 	);
