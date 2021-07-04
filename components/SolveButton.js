ff75c18a6b21dd3c79dca58165b0ec0523a95086
Aryan Chavan <aryan@aryansdomain.com>
2021-07-04 16:56:22 +0530

    add solving and exception handling

--- components/SolveButton.js
+++ components/SolveButton.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Button } from "react-native";
+import solve from "../utils/solver";
 
 function SolveButton({ nVariables, coefficients, constants }) {
 	return (
 		<Button
 			title="solve"
 			onPress={() => {
-				console.log({ nVariables, coefficients, constants });
+				try {
+					const ans = solve(coefficients, constants);
+					console.log(ans);
+				} catch {
+					console.log("invalid input");
+				}
 			}}
 		/>
 	);
